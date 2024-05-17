9d1a2ebcf73e29d7af88b4627df504b29f2471bc
glowfi <86939591+glowfi@users.noreply.github.com>
2024-05-17 11:53:34 +0530

    minor fix

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env fish
 
+# Cleanup
+rm -rf env
+fd . | grep *__pycache__/ | xargs -I "{}" rm -rf "{}"
+
+# Install
 python -m venv env
 source ./env/bin/activate.fish
 pip install fastapi 'strawberry-graphql[fastapi]' uvicorn gunicorn motor beanie python-dotenv
