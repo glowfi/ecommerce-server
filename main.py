e1fe3b76b845a7353f01e9793e61a0de14ebaf56
glowfi <86939591+glowfi@users.noreply.github.com>
2024-06-16 02:26:01 +0530

    testv5

--- main.py
+++ main.py
@@ -69,7 +69,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "PATCH", "DELETE"],
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
