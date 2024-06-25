6e03fc9b1d9818a5efab14d0b075ab82576e598c
glowfi <86939591+glowfi@users.noreply.github.com>
2024-06-26 03:22:35 +0530

    updated README

--- Middleware/jwtmanager.py
+++ Middleware/jwtmanager.py
@@ -8,7 +8,7 @@ from dotenv import find_dotenv, load_dotenv
 load_dotenv(find_dotenv(".env"))
 
 SECRET_KEY = os.getenv("SECRET_KEY")
-ALGORITHM = os.getenv("ALGORITHM")
+JWT_ALGORITHM = os.getenv("JWT_ALGORITHM")
 ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
 
 
@@ -25,14 +25,14 @@ class JWTManager:
                 minutes=float(ACCESS_TOKEN_EXPIRE_MINUTES)
             )
         to_encode.update({"exp": expire})
-        encode_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+        encode_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=JWT_ALGORITHM)
 
         return encode_jwt
 
     @staticmethod
     async def verify_jwt_auth(token: str):
         try:
-            decode_token = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+            decode_token = jwt.decode(token, SECRET_KEY, algorithms=[JWT_ALGORITHM])
             current_timestamp = datetime.utcnow().timestamp()
             if not decode_token:
                 raise ValueError("Invalid token")
@@ -45,7 +45,7 @@ class JWTManager:
     @staticmethod
     async def verify_jwt(token: str):
         try:
-            decode_token = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+            decode_token = jwt.decode(token, SECRET_KEY, algorithms=[JWT_ALGORITHM])
             current_timestamp = datetime.utcnow().timestamp()
             if not decode_token:
                 raise ValueError("Invalid token!")
@@ -58,7 +58,7 @@ class JWTManager:
     @staticmethod
     async def get_token_data(token: str) -> dict | None:
         try:
-            decode_token = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+            decode_token = jwt.decode(token, SECRET_KEY, algorithms=[JWT_ALGORITHM])
             current_timestamp = datetime.utcnow().timestamp()
             if decode_token["exp"] <= current_timestamp:
                 raise ValueError("Token expired!")
--- README.md
+++ README.md
@@ -21,42 +21,4 @@ source ./env/bin/activate
 
 ### Create an .env file in the root directory
 
-Add the below fields with your values in the `.env` file
-
-```sh
-ACCESS_TOKEN_EXPIRE_MINUTES=
-ALGORITHM=""
-DATABASE_URL=""
-FRONTEND_URL=""
-KAFKA_bootstrap_servers=""
-KAFKA_MAIL_TOPIC=""
-KAFKA_sasl_mechanism=""
-KAFKA_sasl_plain_password=""
-KAFKA_sasl_plain_username=""
-KAFKA_security_protocol=""
-MAIL_EMAIL=""
-MAIL_HOST=""
-MAIL_PASSWORD=""
-MAIL_PORT=587
-MAIL_USERNAME=""
-MONGODB_ALTAS_CLUSTER=""
-MONGODB_ALTAS_PRIVATE_KEY=""
-MONGODB_ALTAS_PROJECT_ID=""
-MONGODB_ALTAS_PUBLIC_KEY=""
-MONGODB_DATABASE=""
-OTP_TOKEN_EXPIRE_MINUTES=1440
-RANDOMMER_KEY=""
-RAZORPAY_KEY_ID=""
-RAZORPAY_KEY_SECRET=""
-REDIS_URL=""
-REFRESH_TOKEN_EXPIRE_MINUTES=14400
-RENDER_EXTERNAL_HOSTNAME=""
-RENDER_EXTERNAL_URL=""
-SECRET_KEY=""
-SECRET_REQ_RES=""
-STAGE=""
-STORE_NAME=""
-TOP_RESULTS_SEARCH=6
-USER_AUTOCOMPLETE_INDEX_NAME=""
-USER_SEARCH_INDEX_NAME=""
-```
+Add the environment variables required by creating a `.env` file by taking help from `.env.sample`
