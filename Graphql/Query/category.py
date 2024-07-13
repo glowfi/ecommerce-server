c5bb33d4da4f1936dbe67ff62f66e1d43bd7d852
glowfi <86939591+glowfi@users.noreply.github.com>
2024-07-13 21:14:43 +0530

    added a test query

--- Graphql/Query/category.py
+++ Graphql/Query/category.py
@@ -6,6 +6,11 @@ from models.dbschema import Category
 @strawberry.type
 class Query:
     @strawberry.field
+    def hello_list(self) -> list[int]:
+        print("Hit!")
+        return [1, 2, 3, 4, 5, 6, 7, 8, 5000, 500]
+
+    @strawberry.field
     async def get_all_categories(self, info: strawberry.Info) -> rgca:
 
         try:
